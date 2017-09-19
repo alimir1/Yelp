03aabae4c2808666ded37f4925eaff0b8d498954
alimir1 <contact@alimir.io>
2017-09-19 14:36:20 -0700

    infinitive trolling + refreshCtrl

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -14,6 +14,10 @@ class BusinessesViewController: UIViewController {
     
     var businesses = [Business]()
     
+    var isDownloadingMoreData = false
+
+    var refreshControl: UIRefreshControl!
+    
     var searchBar: UISearchBar!
 
     override func viewDidLoad() {
@@ -32,11 +36,47 @@ class BusinessesViewController: UIViewController {
         
         performSearch()
         
+        self.refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshSearch), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        
+        // Footer view for activity indicator (infinite scrolling)
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 50))
+        let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        activityIndicatorView.center = footerView.center
+        activityIndicatorView.startAnimating()
+        footerView.addSubview(activityIndicatorView)
+        tableView.tableFooterView = footerView
+        
 //        Business.searchWithTerm(term: "Restaurants", sort: .distance, categories: ["asianfusion", "burgers"], deals: true) {
 //            (businesses, error) in
 //            guard let businesses = businesses else { return }
 //            self.businesses = businesses
 //        }
+    }
+    
+    
+    // FIXME: Need to fetch more data from same keyword
+    func performMoreSearch() {
+        isDownloadingMoreData = true
+        Business.searchWithTerm(term: searchBar.text ?? "") {
+            (businesses, error) in
+            guard let businesses = businesses else { return }
+            for business in businesses {
+                self.businesses.append(business)
+            }
+            self.tableView.reloadData()
+            self.isDownloadingMoreData = false
+        }
+    }
+    
+    
+    func refreshSearch(sender: UIRefreshControl) {
+        var term = ""
+        if let searchTerm = searchBar.text {
+            term = searchTerm
+        }
+        performSearch(with: term)
     }
     
     func performSearch(with term: String = "") {
@@ -45,10 +85,27 @@ class BusinessesViewController: UIViewController {
             guard let businesses = businesses else { return }
             self.businesses = businesses
             self.tableView.reloadData()
+            if self.refreshControl.isRefreshing {
+                self.refreshControl.endRefreshing()
+            }
         }
     }
-    
 }
+
+
+// MARK: - Infinite scrolling
+
+extension BusinessesViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard !isDownloadingMoreData else { return }
+        
+        if scrollView.contentOffset.y > scrollView.contentSize.height - tableView.bounds.height && tableView.isDragging {
+                performMoreSearch()
+        }
+    }
+}
+
+// MARK: - UISearchBar delegate
 
 extension BusinessesViewController: UISearchBarDelegate {
     
@@ -64,6 +121,7 @@ extension BusinessesViewController: UISearchBarDelegate {
     
 }
 
+// MARK: - UITableView dataSource and delegate
 
 extension BusinessesViewController: UITableViewDelegate, UITableViewDataSource {
     
