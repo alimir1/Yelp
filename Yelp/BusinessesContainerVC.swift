4546e41d430c4bb35d9db43bf480615f88c5f2f6
alimir1 <contact@alimir.io>
2017-09-24 14:17:58 -0700

    minor change

--- Yelp/BusinessesContainerVC.swift
+++ Yelp/BusinessesContainerVC.swift
@@ -93,7 +93,7 @@ class BusinessesContainerVC: UIViewController {
     }
     
     func searchBarSetup() {
-        searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 800, height: 30))
+        searchBar = UISearchBar()
         searchBar.sizeToFit()
         navigationItem.titleView = searchBar
     }
